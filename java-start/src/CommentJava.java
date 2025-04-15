e73ad58f4909fa96dcadd149c7e67bff948425ea
hagyeong <hathang16@naver.com>
2025-04-15 21:45:10 +0900

    자바 출력과 주석에 대한 코드와 설명
    System.out.println 과 // 과 /* */ 에 대한 강의 듣기

---
+++ java-start/src/CommentJava.java
@@ -0,0 +1,23 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello java1"); // hello java1 출력
+        //System.out.println("Hello java2");
+
+        /*
+        System.out.println("Hello java3");
+        System.out.println("Hello java4");
+        */
+    }
+}
+
+
+// 한 줄 주석 (single line comment)
+// "//" 기호로 시작. 이 기호 이후의 모든 텍스트는 주석으로 처리 됨
+
+// 여러 줄 주석 (multi line comment)
+// "/*로 시작하고 */로 끝남. 이 사이의 모든 텍스트는 주석으로 처리 됨
+
+// 꿀팁
+// psvm : public static void main(String[] args)
+// sout : System.out.println();
---
+++ java-start/src/HelloJava.java
@@ -0,0 +1,19 @@
+public class HelloJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello java");
+    }
+}
+
+// public class HelloJava
+// HelloJava를 클래스라 함
+// 파일명과 클래스 명은 같아야함
+// {}를 사용하여 클래스의 시작과 끝을 나타냄
+
+// public static void main(String[] args)
+// main 메서드라고 함
+// 자바는 main(String[] args)메서드를 찾아 프로그램 실행
+
+// System.out.println("Hello java");
+// System.out.println() : 값을 콘솔에 출력하는 기능
+// 자바는 문자열을 사용할 때 ""를 사용함
