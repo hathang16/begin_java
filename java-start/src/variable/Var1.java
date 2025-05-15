501c65b2ac33cd8a553bc5fda4f871adf44283b0
hagyeong <hathang16@naver.com>
2025-05-15 23:21:20 +0900

    패키지와 관련된 주석 추가

--- java-start/src/variable/Var1.java
+++ java-start/src/variable/Var1.java
@@ -7,3 +7,11 @@ public class Var1 {
         System.out.println(10);
     }
 }
+
+/*
+패키지(package)
+
+- 지금 만든 패키지는 자바 파일을 구분하기 위한 폴더로 우선이해
+- vriable라는 패키지를 만들었다면, 해당 패키지에 들어가는 자바 파일 첫 줄에는 package variable; 와 같이 소속된 패키지를 선언 해줘야함
+- 자바 파일이 위치하는 패키지와 package variable 선언 위치가 같아야함
+*/
