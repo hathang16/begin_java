b5b1ecc640a5a559d27e9253b0ea2be97441b9f1
hagyeong <hathang16@naver.com>
2025-05-16 22:16:52 +0900

    변수를 선언 하는 2가지 방법
    따로 하나씩 선언 하거나, 한번에 여러 변수 선언하기

---
+++ java-start/src/variable/Var4.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Var4 {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        // 변수를 하나씩 선언 하거나
+
+        int c,d;
+        // 한번에 여러 변수를 선언 가능
+    }
+}
