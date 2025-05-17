a1859ce488bb1c1ae8e986b203a54f848c8518fa
hagyeong <hathang16@naver.com>
2025-05-17 22:53:51 +0900

    변수 초기화를 하지 않음으로 인한 오류 해결
    System.out.println(a); 주석처리

--- java-start/src/variable/Var6.java
+++ java-start/src/variable/Var6.java
@@ -4,7 +4,7 @@ public class Var6 {
 
     public static void main(String[] args) {
         int a;
-        System.out.println(a);
+        //System.out.println(a);
 
         //java: variable a might not have been initialized 에러 발생
         //변수 초기화가 되지 않았다는 오류
