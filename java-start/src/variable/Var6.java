e3e0570c7f32388080f7be90da11ff00df818c89
hagyeong <hathang16@naver.com>
2025-05-17 22:51:23 +0900

    변수를 초기화 하지 않았을 때의 예제
    java: variable a might not have been initialized라는 오류 발생

---
+++ java-start/src/variable/Var6.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Var6 {
+
+    public static void main(String[] args) {
+        int a;
+        System.out.println(a);
+
+        //java: variable a might not have been initialized 에러 발생
+        //변수 초기화가 되지 않았다는 오류
+        //자바는 변수 초기화를 강제함
+    }
+}
