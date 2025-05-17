281077352d32a9e87446cdee49638d2019ebda28
hagyeong <hathang16@naver.com>
2025-05-17 22:46:59 +0900

    변수 초기하 방법 예제
    3. 여러 변수 선언과 초기화를 한번에

--- java-start/src/variable/Var5.java
+++ java-start/src/variable/Var5.java
@@ -9,5 +9,10 @@ public class Var5 {
         System.out.println(a);
 
         int b = 2; //2. 변수 선언과 초기화를 한번에
+        System.out.println(b);
+
+        int c = 3, d = 4; //3. 여러 변수 선언과 초기화를 한번에
+        System.out.println(c);
+        System.out.println(d);
     }
 }
