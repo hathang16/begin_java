fe432ead973bf9c27f22b47ca616a165fe8fcc72
hagyeong <hathang16@naver.com>
2025-05-17 22:43:53 +0900

    변수 초기화 방법 예제
    1. 변수 선언, 초기화 따로

---
+++ java-start/src/variable/Var5.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var5 {
+
+    public static void main(String[] args) {
+        //1 변수 선언, 초기화 따로
+        int a;
+        a = 1;
+        System.out.println(a);
+    }
+}
